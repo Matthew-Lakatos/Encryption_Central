56978bd7f91841baa88cedc6997a190d7c0e6410
Matthew Lakatos <167357621+De-Imam@users.noreply.github.com>
2025-04-30 20:56:57 +0100

    Update Caesar_Cipher.py

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,24 +1,55 @@
 # Caesar Cipher code class
-class Solution():
-  self.char_map = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
-  def __init__(self):
-    self.plaintext = input("Enter the text you would like to encrypt:\n")
-    self.shift_chars = int(input("Enter the number of places you would like to shift each character by by:\n")) % 26
-    try:
-      self.shift_pos = int(input("Enter the number of places you would like to shift each character's position by:\n")) % len(self.plaintext)
-    except:
-      self.shift_pos = 0
-  def encode(self):
-    ciphertext_nonshift = ""
-      try: 
-        ciphertext_nonshift += str(char_map.find(self.char_map[element] + self.shift_pos))
-      except:
-        ciphertext_nonshift += str(element)
-    print(ciphertext_nonshift)
-    ciphertext = [0] * len(ciphertext_nonshift)
-    for i in xrange(len(ciphertext_nonshift)):
-      ciphertext[- i - self.shift_pos] = ciphertext_nonshift[i]
-    print(ciphertext)
-    print(''.join(ciphertext))
-    return ''.join(ciphertext)
-    
+class Solution:
+    def __init__(self):
+        self.plaintext = input("Enter the text you would like to encrypt:\n")
+        self.shift_chars = int(input("Enter the number of places you would like to shift each character by:\n")) % 26
+        try:
+            self.shift_pos = int(input("Enter the number of places you would like to shift each character's position by:\n")) % len(self.plaintext)
+        except:
+            self.shift_pos = 0
+
+    def encode(self):
+        ciphertext_nonshift = ""
+
+        for char in self.plaintext:
+            if char.isalpha():
+                if char.isupper():
+                    base = ord('A')
+                else:
+                    base = ord('a')
+                shifted = (ord(char) - base + self.shift_chars) % 26
+                ciphertext_nonshift += chr(base + shifted)
+            else:
+                ciphertext_nonshift += char
+
+        # Shift positions of characters
+        ciphertext = [''] * len(ciphertext_nonshift)
+        for i in range(len(ciphertext_nonshift)):
+            new_index = (i + self.shift_pos) % len(ciphertext_nonshift)
+            ciphertext[new_index] = ciphertext_nonshift[i]
+
+        encrypted = ''.join(ciphertext)
+        print("Encrypted text:", encrypted)
+        return encrypted
+      
+    def decode(self, ciphertext):
+        # Step 1: Undo position shift (rotate in the opposite direction)
+        n = len(ciphertext)
+        shifted_back = [''] * n
+        for i in range(n):
+            original_index = (i - self.shift_pos) % n
+            shifted_back[original_index] = ciphertext[i]
+        unshifted_text = ''.join(shifted_back)
+
+        # Step 2: Undo Caesar character shift
+        decrypted = ""
+        for char in unshifted_text:
+            if char.isalpha():
+                base = ord('A') if char.isupper() else ord('a')
+                shifted = (ord(char) - base - self.shift_chars) % 26
+                decrypted += chr(base + shifted)
+            else:
+                decrypted += char
+
+        print("Decrypted text:", decrypted)
+        return decrypted
