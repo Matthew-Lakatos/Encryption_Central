e275ca42d3a450eae3befdccadec46a2b4bdfa36
Matthew Lakatos <167357621+De-Imam@users.noreply.github.com>
2025-05-03 17:24:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-from Caesar_Cipher import *
-sol = Solution()
-sol.encode()
+import time as t
+import sys
+
+import Caesar_Cipher
+import Hill_Cipher
+import Vigenere_Cipher
+import One_Time_Pad_Cipher
+
+going = True
+while going == True:
+  type = input('''  Enter the encryption system you would like to use:
+                                  Caesar Cipher - input 1
+                                 Vigenere Cipher - input 2
+                               One-Time Pad Cipher - input 3
+                                   Hill Cipher - input 4 
+                           (Or anything else to exit the program)''')
+  if type == '1':
+    # implement Caesar Cipher
+  elif type == '2':
+    # implement Vigenere Cipher
+  elif type == '3':
+    # implement OTP Cipher
+  elif type == '4':
+    # implement Hill Cipher
+  else:
+    print("Thank you for using my Encryption program")
+    t.sleep(1)
+    sys.exit()
